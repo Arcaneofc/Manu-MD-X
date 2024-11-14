1b524c5c5e465531b998a549822daeff76282a41
Arcaneofc <154892039+Arcaneofc@users.noreply.github.com>
2024-11-15 00:28:09 +0500

    Update config.js

--- config.js
+++ config.js
@@ -43,11 +43,11 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "put-your-session-id",
+SESSION_ID: process.env.SESSION_ID || "Manu-MD&zQNiASQJ#mDRQCmU2HPINQJPSZdi8a_2N1zVotnxjqqg978-EeOs",
 ALIVE_IMG : process.env.ALIVE_IMG || "https://telegra.ph/file/ef48cc8d2d26081f7d46a.jpg",
 ALIVE_MSG : process.env.ALIVE_MSG || "Put your alive messege",
 MENU_IMG : process.env.MENU_IMG || "https://telegra.ph/file/ef48cc8d2d26081f7d46a.jpg",
-MODE : process.env.MODE || "public",
+MODE : process.env.MODE || "private",
 WELCOME : process.env.WELCOME || "false",
 ANTI_PHOTO: process.env.ANTI_PHOTO || "false",
 ANTI_STICKER: process.env.ANTI_STICKER || "false",
@@ -67,7 +67,7 @@ ANTI_DELETE: process.env.ANTI_DELETE || "true",
 ALWAYS_ONLINE : process.env.ALWAYS_ONLINE || "false",
 AUTO_TYPING : process.env.AUTO_TYPING || "false",
 AUTO_RECORDING : process.env.AUTO_RECORDING || "false",
-OWNER_NUMBER : process.env.OWNER_NUMBER || "94742274855",
+OWNER_NUMBER : process.env.OWNER_NUMBER || "923464213708",
 DELCHAT : process.env.DELCHAT || "pm",
 FOOT : process.env.FOOT || "🧚‍♀️𝗠𝗔𝗡𝗨-𝗠𝗗-𝗩❼💗",
 BOT_INFO: process.env.BOT_INFO || "Manu-MD;OFC;0;https://i.ibb.co/GRpbTZp/IMG-20240920-WA0020.jpg;https://whatsapp.com/channel/0029VaN1XMn2ZjCsu9eZQP3R",
